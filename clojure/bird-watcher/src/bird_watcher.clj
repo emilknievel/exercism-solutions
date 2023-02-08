cc91d29466c870d97f8d31a43def1c6847106cef
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-02-08 21:32:56 +0100

    Clojure: bird-watcher wip[4/7]

---
+++ clojure/bird-watcher/src/bird_watcher.clj
@@ -0,0 +1,22 @@
+(ns bird-watcher)
+
+(def last-week 
+  [0 2 5 3 7 8 4])
+
+(defn today [birds]
+  (last birds))
+
+(defn inc-bird [birds]
+  (conj (drop-last birds) (inc (today birds))))
+
+(defn day-without-birds? [birds]
+  (contains? birds 0))
+
+(defn n-days-count [birds n]
+  )
+
+(defn busy-days [birds]
+  )
+
+(defn odd-week? [birds]
+  )
