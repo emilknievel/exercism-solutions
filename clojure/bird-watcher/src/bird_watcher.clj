3c0eb9d46177a999cf4dabb5bcb9c0729291b60c
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-02-12 02:35:40 +0100

    Clojure: bird-watcher - solution.

--- clojure/bird-watcher/src/bird_watcher.clj
+++ clojure/bird-watcher/src/bird_watcher.clj
@@ -4,19 +4,22 @@
   [0 2 5 3 7 8 4])
 
 (defn today [birds]
-  (last birds))
+  (peek birds))
 
 (defn inc-bird [birds]
-  (conj (drop-last birds) (inc (today birds))))
+  (conj (pop birds) (inc (today birds))))
 
 (defn day-without-birds? [birds]
-  (contains? birds 0))
+  (some? (some (fn [item] (= item 0)) birds)))
 
 (defn n-days-count [birds n]
   (reduce + (take n birds)))
 
 (defn busy-days [birds]
-  )
+  (count
+    (filter identity
+            (map (fn [item] (> item 4))
+                 birds))))
 
 (defn odd-week? [birds]
-  )
+  (= [1 0 1 0 1 0 1] birds))
