2af9971ccff175783d76c7d2976b782755cc0f38
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-02-13 21:43:44 +0100

    Clojure: cars-assemble - solution [1/2].

--- clojure/cars-assemble/src/cars_assemble.clj
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -1,10 +1,18 @@
 (ns cars-assemble)
 
+(def cars 221)
+
 (defn production-rate
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
-  )
+  (cond (= speed 0) 0.0
+        (and (> speed 0)
+             (< speed 5)) (float (* speed cars))
+        (and (> speed 4)
+             (< speed 9)) (* speed cars 0.9)
+        (= speed 9) (* speed cars 0.8)
+        (= speed 10) (* speed cars 0.77)))
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
