277f89353a04134fd26d3d6cc61ebb0a9e91f4be
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-02-19 22:17:58 +0100

    Clojure: interest-is-interesting wip[3/3].

--- clojure/interest-is-interesting/src/interest_is_interesting.clj
+++ clojure/interest-is-interesting/src/interest_is_interesting.clj
@@ -1,16 +1,24 @@
 (ns interest-is-interesting)
 
 (defn interest-rate
-  "TODO: add docstring"
   [balance]
-  )
+  (condp > balance
+    0    -3.213
+    1000 0.5
+    5000 1.621
+    2.475))
 
+;; FIXME: Seems to use the wrong amount of decimals when calculating
 (defn annual-balance-update
-  "TODO: add docstring"
   [balance]
-  )
+  (+ balance (* balance (bigdec (/ (interest-rate balance) 100)))))
+
+(defn donate-big-amount
+  [balance tax-free-percentage]
+  (int (* balance (/ (* tax-free-percentage 2) 100))))
 
 (defn amount-to-donate
-  "TODO: add docstring"
   [balance tax-free-percentage]
-  )
+  (if (pos? balance)
+    (donate-big-amount balance tax-free-percentage)
+    0))
