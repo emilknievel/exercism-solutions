3e5f6da57de4d273a0857bc6fed86cfbe56dad8f
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-03-30 22:42:47 +0200

    Rust: gigasecond - solution.

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
 use time::PrimitiveDateTime as DateTime;
+use time::Duration;
 
 // Returns a DateTime one billion seconds after start.
+// 1Gs = 1,000,000,000s
 pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {start}");
+    start + Duration::seconds(1000000000)
 }
