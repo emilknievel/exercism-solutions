4f84ed8ac0f01a7633ed58800b67b623611209a1
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-03-31 19:13:11 +0200

    Rust: armstrong-numbers - solution, it2.

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {num} is an armstrong number")
+    let num = num as u64;
+    let mut sum: u64 = 0;
+
+    let digits: Vec<u32> = num
+        .to_string()
+        .chars()
+        .take_while(|c| c.is_numeric())
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+
+    for digit in &digits {
+        sum = sum.wrapping_add(digit.pow(digits.len().try_into().unwrap()).into());
+    }
+
+    return num == sum;
 }
