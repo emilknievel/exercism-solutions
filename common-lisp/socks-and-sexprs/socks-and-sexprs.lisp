4d102ba5557347cb065d6389879dbfa534511ae2
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-06-13 23:18:32 +0200

    common-lisp(socks-and-sexprs): solution.

--- common-lisp/socks-and-sexprs/socks-and-sexprs.lisp
+++ common-lisp/socks-and-sexprs/socks-and-sexprs.lisp
@@ -6,31 +6,23 @@
 (in-package :socks-and-sexprs)
 
 ;; Evaluates to some symbol (not a keyword)
-(defun lennys-favorite-food ()
-  ;; put your symbol here
-  )
+(defun lennys-favorite-food () 'lasagna)
 
 ;; Evaluates to some keyword
-(defun lennys-secret-keyword ()
-  ;; put your keyword here
-  )
+(defun lennys-secret-keyword () :aliens-are-real)
 
 ;; Evaluates to T if THING is an atom, NIL otherwise
 (defun is-an-atom-p (thing)
-  ;; put the code to determine if THING is an atom here
-  )
+  (atom thing))
 
 ;; Evaluates to T if THING is a cons, NIL otherwise
 (defun is-a-cons-p (thing)
-  ;; put the code to determine if THING is a CONS here
-  )
+  (consp thing))
 
 ;; Evaluates to the first part of CONS
 (defun first-thing (cons)
-  ;; put the code to get the first part of CONS here
-  )
+  (car cons))
 
 ;; Evaluates to the 'rest' of the CONS
 (defun rest-of-it (cons)
-  ;; put the code to get the rest of CONS here
-  )
+  (cdr cons))
