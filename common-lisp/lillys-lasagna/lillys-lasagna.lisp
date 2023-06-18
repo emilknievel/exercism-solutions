79501deb5ed8286a3be5fb08d7a0cdfdc17ee7ce
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-06-18 19:40:46 +0200

    common-lisp(lillys-lasagna): solution.

--- common-lisp/lillys-lasagna/lillys-lasagna.lisp
+++ common-lisp/lillys-lasagna/lillys-lasagna.lisp
@@ -7,10 +7,18 @@
 
 (in-package :lillys-lasagna)
 
-;; Define function expected-time-in-oven
+(defun expected-time-in-oven ()
+  "How many minutes the lasagna should be in the oven"
+  337)
 
-;; Define function remaining-minutes-in-oven
+(defun remaining-minutes-in-oven (minutes-spent)
+  "How many minutes the lasagna still has to remain in the oven"
+  (- (expected-time-in-oven) minutes-spent))
 
-;; Define function preparation-time-in-minutes
-
-;; Define function elapsed-time-in-minutes
+(defun preparation-time-in-minutes (layers)
+  "How many minutes that have been spent preparing the lasagna"
+  (* 19 layers))
+  
+(defun elapsed-time-in-minutes (layers time-in-oven)
+  "How many minutes have been spent cooking the lasagna"
+  (+ (preparation-time-in-minutes layers) time-in-oven))
