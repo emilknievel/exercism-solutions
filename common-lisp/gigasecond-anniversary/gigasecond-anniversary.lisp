0b76fd756e44eeaf0d5c95f57b647607f312aeb0
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-06-18 22:04:17 +0200

    common-lisp(gigasecond-anniversary): solution.

--- common-lisp/gigasecond-anniversary/gigasecond-anniversary.lisp
+++ common-lisp/gigasecond-anniversary/gigasecond-anniversary.lisp
@@ -3,4 +3,11 @@
   (:export :from))
 (in-package :gigasecond-anniversary)
 
-(defun from (year month day hour minute second))
+(defun from-universal (year month day hour minute second)
+  (+ (encode-universal-time second minute hour day month year 0) (expt 10 9)))
+
+(defun from (year month day hour minute second)
+  (multiple-value-bind
+        (sec min hr dy mth yr)
+      (decode-universal-time (from-universal year month day hour minute second))
+    (list yr mth dy hr min sec)))
