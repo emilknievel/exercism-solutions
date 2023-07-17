113abaa1491376ae8f774525c59cbeffe5dea7b8
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-07-17 14:41:16 +0200

    c(isogram): solution

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -1,1 +1,21 @@
 #include "isogram.h"
+#include "ctype.h"
+#include "stdlib.h"
+
+bool is_isogram(const char *phrase) {
+    if (phrase == NULL) {
+        return false;
+    }
+
+    for (int i = 0; phrase[i] != '\0'; ++i) {
+        for (int j = i; j >= 0; --j) {
+            if (&phrase[i - j] != &phrase[i] &&
+                tolower(phrase[i - j]) == tolower(phrase[i]) &&
+                phrase[i] != '-' && phrase[i] != ' ') {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
