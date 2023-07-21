10b67db05abc2dffd21fb98cda9a8c2ab501b511
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-07-21 19:42:58 +0200

    c(grains): solution - iter1
    Solution using pow() function.

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,1 +1,17 @@
 #include "grains.h"
+#include <math.h>
+#include <stdint.h>
+
+uint64_t square(uint8_t index) {
+    return (index == 0 || index > 64) ? 0 : pow(2, index - 1);
+}
+
+uint64_t total(void) {
+    uint64_t sum = 0;
+
+    for (int i = 1; i < 65; ++i) {
+        sum += square(i);
+    }
+
+    return sum;
+}
