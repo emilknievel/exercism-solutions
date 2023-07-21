17363120a990d999ffccf262257169cfc2a1b8b3
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-07-21 13:52:52 +0200

    c(hamming): solution - iter2
    [Pointer incrementation approach](https://exercism.org/tracks/c/exercises/hamming/dig_deeper).

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -2,16 +2,29 @@
 #include <string.h>
 
 int compute(const char *lhs, const char *rhs) {
-    if (strlen(lhs) != strlen(rhs)) {
+    if (!lhs || !rhs)
         return -1;
+
+    int distance = 0;
+    for (; *lhs && *rhs; ++lhs, ++rhs) {
+        if (*lhs != *rhs)
+            ++distance;
     }
 
-    int hamming = 0;
-    for (int i = 0; i < (int)strlen(lhs); ++i) {
-        if (lhs[i] != rhs[i]) {
-            ++hamming;
-        }
-    }
-
-    return hamming;
+    return (*lhs || *rhs) ? -1 : distance;
 }
+
+// int compute(const char *lhs, const char *rhs) {
+//     if (strlen(lhs) != strlen(rhs)) {
+//         return -1;
+//     }
+//
+//     int hamming = 0;
+//     for (int i = 0; i < (int)strlen(lhs); ++i) {
+//         if (lhs[i] != rhs[i]) {
+//             ++hamming;
+//         }
+//     }
+//
+//     return hamming;
+// }
