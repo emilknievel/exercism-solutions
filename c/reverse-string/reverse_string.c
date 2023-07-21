4132ec7b7d292d0aa321429cdf5cdac8526e13e0
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-07-21 20:24:25 +0200

    c(reverse-string): solution - iter1

--- c/reverse-string/reverse_string.c
+++ c/reverse-string/reverse_string.c
@@ -1,1 +1,14 @@
 #include "reverse_string.h"
+#include <stdlib.h>
+#include <string.h>
+
+char * reverse(const char * value) {
+    int length = strlen(value);
+    char * reversed = malloc(length * sizeof(char));
+
+    for (int i = length - 1; i >= 0; --i) {
+        reversed[length - 1 - i] = value[i];
+    }
+
+    return reversed;
+}
