553c25d3497b8d37940f6706c6fe7c310f1956e9
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-08-01 14:22:30 +0200

    javascript(poetry-club-door-policy): solution

--- javascript/poetry-club-door-policy/door-policy.js
+++ javascript/poetry-club-door-policy/door-policy.js
@@ -27,7 +27,17 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  return line[0];
+}
+
+/**
+ * Capitalize word.
+ *
+ * @param {string} word the word to capitalize
+ * @returns {string} the capitalized word
+ */
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
 
 /**
@@ -38,7 +48,7 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  return capitalize(word);
 }
 
 /**
@@ -49,7 +59,7 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  return line.trim().at(-1);
 }
 
 /**
@@ -60,5 +70,5 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  return capitalize(word) + ', please';
 }
