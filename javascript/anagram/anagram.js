a87afc084e6ae2c98a2614545f67d9d097f9ee05
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-08-01 17:41:57 +0200

    javascript(anagram): solution

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,8 +1,31 @@
-//
-// This is only a SKELETON file for the 'Anagram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const getPermutations = (array) => {
+  let permutations = [];
 
-export const findAnagrams = () => {
-  throw new Error('Remove this statement and implement this function');
+  if (array.length === 0) return [];
+  if (array.length === 1) return [array];
+
+  for (let i = 0; i < array.length; ++i) {
+    const currentItem = array[i];
+    const remainingItems = array.slice(0, i).concat(array.slice(i + 1));
+    const remainingItemsPermuted = getPermutations(remainingItems);
+
+    for (let j = 0; j < remainingItemsPermuted.length; ++j) {
+      const permutedArray = [currentItem].concat(remainingItemsPermuted[j]);
+      permutations.push(permutedArray);
+    }
+  }
+
+  return permutations;
+};
+
+export const findAnagrams = (word, candidates) => {
+  let array = Array.from(word);
+  let permutations = getPermutations(array);
+  let anagrams = permutations.map((e) => e.join('').toLowerCase());
+
+  return candidates.filter(
+    (c) =>
+      c.toLowerCase() !== word.toLowerCase() &&
+      anagrams.includes(c.toLowerCase())
+  );
 };
