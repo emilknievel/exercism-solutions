a058695c9e0e400ae3669d2b487bf93744ffb798
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-08-02 22:37:01 +0200

    javascript(lasagna-master): solution

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -1,12 +1,38 @@
 /// <reference path="./global.d.ts" />
 // @ts-check
 
-/**
- * Implement the functions needed to solve the exercise here.
- * Do not forget to export them so they are available for the
- * tests. Here an example of the syntax as reminder:
- *
- * export function yourFunction(...) {
- *   ...
- * }
- */
+export function cookingStatus(timer) {
+  if (timer === undefined) return 'You forgot to set the timer.';
+  if (timer === 0) return 'Lasagna is done.';
+  if (timer > 0) return 'Not done, please wait.';
+}
+
+const preparationTime = (layers, timePerLayer) =>
+  timePerLayer ? layers.length * timePerLayer : layers.length * 2;
+
+const quantities = (layers) => {
+  let q = { noodles: 0, sauce: 0 };
+
+  layers.forEach(layer => {
+    if (layer === 'noodles') q.noodles += 50;
+    else if (layer === 'sauce') q.sauce += 0.2;
+  });
+
+  return q;
+};
+
+const addSecretIngredient = (list1, list2) => {
+  list2.push(list1.at(-1));
+};
+
+const scaleRecipe = (recipe, portionCount) => {
+  let scaledRecipe = {};
+
+  for (const [key, value] of Object.entries(recipe)) {
+    scaledRecipe[key] = (value / 2) * portionCount;
+  }
+
+  return scaledRecipe;
+};
+
+export { preparationTime, quantities, addSecretIngredient, scaleRecipe };
