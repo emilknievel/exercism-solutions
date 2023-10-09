91389a60c6203eec645a2a7f3f4758e88fdac0f7
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-10-09 06:14:57 +0200

    emacs(phone-number-analysis): solution

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -4,11 +4,14 @@ public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        var sections = phoneNumber.Split("-");
+        string dialingCode = sections[0];
+        string prefixCode = sections[1];
+        string lastDigits = sections[2];
+
+        return (dialingCode == "212", prefixCode == "555", lastDigits);
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
-    {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
-    }
+        => phoneNumberInfo.IsFake;
 }
