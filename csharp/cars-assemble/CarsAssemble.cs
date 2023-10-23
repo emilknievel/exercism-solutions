32fb7272496fe892caa387a44880faf4fa9e2ac4
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-10-23 22:47:38 +0200

    csharp(cars-assemble): solution

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -1,19 +1,25 @@
-using System;
-
-static class AssemblyLine
+﻿static class AssemblyLine
 {
     public static double SuccessRate(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.SuccessRate() method");
-    }
-    
-    public static double ProductionRatePerHour(int speed)
-    {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+        switch (speed)
+        {
+            case 0:
+                return 0;
+            case >= 1 and <= 4:
+                return 1.0;
+            case >= 5 and <= 8:
+                return 0.9;
+            case 9:
+                return 0.8;
+            default:
+                return 0.77;
+        }
     }
 
-    public static int WorkingItemsPerMinute(int speed)
-    {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
-    }
+    public static double ProductionRatePerHour(int speed) =>
+        speed * 221 * SuccessRate(speed);
+
+    public static int WorkingItemsPerMinute(int speed) =>
+        (int)(ProductionRatePerHour(speed) / 60);
 }
