71e359970b69a75ab52668d7a3c2601a0c4f5066
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-01-08 06:51:47 +0100

    csharp(interest-is-interesting): solution

--- csharp/interest-is-interesting/InterestIsInteresting.cs
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -1,24 +1,26 @@
-using System;
-
 static class SavingsAccount
 {
-    public static float InterestRate(decimal balance)
+    public static float InterestRate(decimal balance) => balance switch
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.InterestRate() method");
-    }
+        < 0 => 3.213f,
+        < 1000 => .5f,
+        < 5000 => 1.621f,
+        _ => 2.475f
+    };
 
-    public static decimal Interest(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.Interest() method");
-    }
+    public static decimal Interest(decimal balance) => balance * (decimal)InterestRate(balance) / 100;
 
-    public static decimal AnnualBalanceUpdate(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
-    }
+    public static decimal AnnualBalanceUpdate(decimal balance) => balance + Interest(balance);
 
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+        int years = 0;
+        while (balance < targetBalance)
+        {
+            balance = AnnualBalanceUpdate(balance);
+            ++years;
+        }
+
+        return years;
     }
 }
