4bf9533376625f910fb1b1391e2a6d8bbd212e2f
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-01-08 05:44:57 +0100

    csharp(tim-from-marketing): solution

--- csharp/tim-from-marketing/TimFromMarketing.cs
+++ csharp/tim-from-marketing/TimFromMarketing.cs
@@ -1,9 +1,8 @@
-using System;
-
 static class Badge
 {
     public static string Print(int? id, string name, string? department)
     {
-        throw new NotImplementedException("Please implement the (static) Badge.Print() method");
+        string idString = id != null ? $"[{id}] - " : "";
+        return $"{idString}{name} - {department?.ToUpper() ?? "OWNER"}";
     }
 }
