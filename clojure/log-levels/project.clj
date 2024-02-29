ce1950fc93f71ef68b2a0992af9fe72539db66a4
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-02-29 06:00:45 +0100

    clojure: remove untracked
    Remove files that are not part of the solution.

--- clojure/log-levels/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject log-levels "0.1.0-SNAPSHOT"
-  :description "log-levels exercise."
-  :url "https://github.com/exercism/clojure/tree/main/exercises/concept/log-levels"
-  :dependencies [[org.clojure/clojure "1.10.0"]])
