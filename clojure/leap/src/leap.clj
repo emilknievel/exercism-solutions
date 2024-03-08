7ae895d98f167b9c28e552180e047f51b34e323e
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-03-08 22:14:40 +0100

    clojure(leap): solution

--- clojure/leap/src/leap.clj
+++ clojure/leap/src/leap.clj
@@ -1,5 +1,9 @@
 (ns leap)
 
-(defn leap-year? [year] ;; <- argslist goes here
-  ;; your code goes here
-)
+(defn- divides? [number divisor]
+  (zero? (mod number divisor)))
+
+(defn leap-year? [year]
+  (if (divides? year 100)
+    (divides? year 400)
+    (divides? year 4)))
