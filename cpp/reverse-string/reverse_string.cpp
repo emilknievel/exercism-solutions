c117d79ae8406d5f09990d6e8a24c1ca3506b243
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-03-10 16:58:28 +0100

    cpp(reverse-string): solution

---
+++ cpp/reverse-string/reverse_string.cpp
@@ -0,0 +1,11 @@
+#include "reverse_string.h"
+#include <algorithm>
+
+namespace reverse_string
+{
+    std::string reverse_string(std::string str)
+    {
+        std::reverse(str.begin(), str.end());
+        return str;
+    }
+} // namespace reverse_string
