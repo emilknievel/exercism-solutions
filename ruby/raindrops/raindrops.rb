33a20ae79a4cf3fd662494a8d888a5b697a27091
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-03-14 14:02:51 +0100

    ruby(raindrops): solution

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,7 +1,9 @@
-=begin
-Write your code for the 'Raindrops' exercise in this file. Make the tests in
-`raindrops_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/raindrops` directory.
-=end
+module Raindrops
+  def self.convert(n)
+    s = ""
+    s += "Pling" if n % 3 == 0
+    s += "Plang" if n % 5 == 0
+    s += "Plong" if n % 7 == 0
+    s.empty? ? n.to_s : s
+  end
+end
