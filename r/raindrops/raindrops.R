703d9b6402208e8e3affad4157d5e13630136c2a
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-03-15 00:46:40 +0100

    r(raindrops): solution

--- r/raindrops/raindrops.R
+++ r/raindrops/raindrops.R
@@ -1,3 +1,11 @@
 raindrops <- function(number) {
-  
+  str <- ""
+
+  if (number %% 3 == 0) str <- paste0(str, "Pling")
+  if (number %% 5 == 0) str <- paste0(str, "Plang")
+  if (number %% 7 == 0) str <- paste0(str, "Plong")
+
+  if (nchar(str) > 0) return(str)
+
+  return(as.character(number))
 }
