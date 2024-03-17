21b62a9eb3226fdc9dda2e8e3ef243031d8429f3
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-03-17 15:54:36 +0100

    csharp(reverse-string): solution iter 3, linq

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -1,12 +1,6 @@
-using System;
 using System.Linq;
 
 public static class ReverseString
 {
-    public static string Reverse(string input)
-    {
-        var inputArr = input.ToCharArray();
-        Array.Reverse(inputArr);
-        return new string(inputArr);
-    }
+    public static string Reverse(string input) => new(input.Reverse().ToArray());
 }
