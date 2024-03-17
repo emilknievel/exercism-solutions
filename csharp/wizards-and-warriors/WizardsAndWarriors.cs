0d96cafe240ab24cd93ef8c4331179cf6bbedd13
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-03-17 17:24:03 +0100

    csharp(wizards-and-warriors): solution iter2

--- csharp/wizards-and-warriors/WizardsAndWarriors.cs
+++ csharp/wizards-and-warriors/WizardsAndWarriors.cs
@@ -1,37 +1,22 @@
-using System;
-
 abstract class Character
 {
-    private readonly string _characterType;
-
-    protected Character(string characterType)
-    {
-        _characterType = characterType;
-    }
+    protected string CharacterType => GetType().Name;
 
     public abstract int DamagePoints(Character target);
 
     public virtual bool Vulnerable() => false;
 
-    public override string ToString() => $"Character is a {_characterType}";
+    public override string ToString() => $"Character is a {CharacterType}";
 }
 
 class Warrior : Character
 {
-    public Warrior() : base("Warrior")
-    {
-    }
-
     public override int DamagePoints(Character target) => target.Vulnerable() ? 10 : 6;
 }
 
 class Wizard : Character
 {
     private bool _spellPrepared;
-
-    public Wizard() : base("Wizard")
-    {
-    }
 
     public override int DamagePoints(Character target) => _spellPrepared ? 12 : 3;
 
