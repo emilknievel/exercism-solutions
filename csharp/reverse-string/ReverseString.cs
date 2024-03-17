1de9e1ad6e06e30bcb943996acabbf5025627782
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-03-17 15:45:29 +0100

    csharp(reverse-string): solution

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,15 @@
+using System.Text;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        var reversed = new StringBuilder(input.Length);
+        for (var i = input.Length-1; i >= 0; i--)
+        {
+            reversed.Append(input[i]);
+        }
+
+        return reversed.ToString();
+    }
+}
