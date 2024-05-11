722445ce0ba6d0ba3a726781a66da1f5286edd03
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-05-11 20:22:03 +0200

    csharp(rotational-cipher): solution 2

--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -1,38 +1,17 @@
 using System;
-using System.Text;
+using System.Linq;
 
 public static class RotationalCipher
 {
-    public static string Rotate(string text, int shiftKey)
+    public static string Rotate(string text, int shiftKey) => shiftKey is < 0 or > 26
+            ? throw new ArgumentOutOfRangeException($"Key value {shiftKey} is outside the allowed range: `0 <= k <= 26`")
+            : new string(text.Select(c => Rotate(c, shiftKey)).ToArray());
+
+    private static char Rotate(char c, int shiftKey)
     {
-        if (shiftKey is < 0 or > 26)
-        {
-            throw new ArgumentOutOfRangeException($"Key value {shiftKey} is outside the allowed range: `0 <= k <= 26`");
-        }
+        if (!char.IsLetter(c)) return c;
 
-        var alphabet = "abcdefghijklmnopqrstuvwxyz";
-        var alphabetCap = alphabet.ToUpperInvariant();
-
-        StringBuilder sb = new();
-        foreach (char c in text)
-        {
-            var index = alphabet.IndexOf(c);
-            if (index < 0)
-            {
-                var cUpper = char.ToUpperInvariant(c);
-                index = alphabetCap.IndexOf(cUpper);
-                if (index < 0)
-                {
-                    sb.Append(c);
-                    continue;
-                }
-                sb.Append(GetRotChar(alphabetCap, cUpper, shiftKey));
-                continue;
-            }
-            sb.Append(GetRotChar(alphabet, c, shiftKey));
-        }
-        return sb.ToString();
+        var start = char.IsLower(c) ? 'a' : 'A';
+        return (char)(start + (c - start + shiftKey) % 26);
     }
-
-    private static char GetRotChar(string s, char c, int shiftKey) => s[(s.IndexOf(c) + shiftKey) % 26];
 }
