ce50728b0abb8c8ebf656e072ba3747a2774e345
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-06-20 19:57:38 +0200

    csharp(beauty-salon-goes-global): solution

--- csharp/beauty-salon-goes-global/BeautySalonGoesGlobal.cs
+++ csharp/beauty-salon-goes-global/BeautySalonGoesGlobal.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
 
 public enum Location
 {
@@ -18,25 +21,72 @@ public static class Appointment
 {
     public static DateTime ShowLocalTime(DateTime dtUtc) => dtUtc.ToLocalTime();
 
-    public static DateTime Schedule(string appointmentDateDescription, Location location)
-    {
-        throw new NotImplementedException(
-            "Please implement the (static) Appointment.Schedule() method"
-        );
-    }
+    public static DateTime Schedule(string appointmentDateDescription, Location location) =>
+        TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse(appointmentDateDescription), GetLocationTimeZoneInfo(location));
 
-    public static DateTime GetAlertTime(DateTime appointment, AlertLevel alertLevel)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.GetAlertTime() method");
-    }
+    public static DateTime GetAlertTime(DateTime appointment, AlertLevel alertLevel) =>
+        alertLevel switch
+        {
+            AlertLevel.Early => appointment.AddDays(-1),
+            AlertLevel.Standard => appointment.AddHours(-1).AddMinutes(-45),
+            AlertLevel.Late => appointment.AddMinutes(-30),
+            _ => throw new ArgumentOutOfRangeException(
+                nameof(appointment),
+                $"Unexpected appointment value: {appointment}"
+            )
+        };
 
-    public static bool HasDaylightSavingChanged(DateTime dt, Location location)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.HasDaylightSavingChanged() method");
-    }
+    public static bool HasDaylightSavingChanged(DateTime dt, Location location) =>
+        GetLocationTimeZoneInfo(location).IsDaylightSavingTime(dt) !=
+        GetLocationTimeZoneInfo(location).IsDaylightSavingTime(dt.AddDays(-7));
 
     public static DateTime NormalizeDateTime(string dtStr, Location location)
     {
-        throw new NotImplementedException("Please implement the (static) Appointment.NormalizeDateTime() method");
+        return DateTime.TryParse(dtStr, GetLocationCultureInfo(location), out var normalizedDateTime)
+            ? normalizedDateTime
+            : new DateTime(1, 1, 1);
+    }
+
+    private static TimeZoneInfo GetLocationTimeZoneInfo(Location location)
+    {
+        var locationsOsxLinux = new Dictionary<Location, string>()
+        {
+            { Location.NewYork, "America/New_York" },
+            { Location.London, "Europe/London" },
+            { Location.Paris, "Europe/Paris" }
+        };
+
+        var locationsWindows = new Dictionary<Location, string>()
+        {
+            { Location.NewYork, "Eastern Standard Time" },
+            { Location.London, "GMT Standard Time" },
+            { Location.Paris, "W. Europe Standard Time" }
+        };
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return TimeZoneInfo.FindSystemTimeZoneById(locationsWindows[location]);
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            return TimeZoneInfo.FindSystemTimeZoneById(locationsOsxLinux[location]);
+        }
+
+        throw new PlatformNotSupportedException(
+            $"Platforms other than {OSPlatform.Windows}, {OSPlatform.Linux} or {OSPlatform.OSX} are not supported."
+        );
+    }
+
+    private static CultureInfo GetLocationCultureInfo(Location location)
+    {
+        var locationCultureInfoDict = new Dictionary<Location, CultureInfo>()
+        {
+            { Location.NewYork, new CultureInfo("en-US") },
+            { Location.London, new CultureInfo("en-GB") },
+            { Location.Paris, new CultureInfo("fr-FR") }
+        };
+
+        return locationCultureInfoDict[location];
     }
 }
