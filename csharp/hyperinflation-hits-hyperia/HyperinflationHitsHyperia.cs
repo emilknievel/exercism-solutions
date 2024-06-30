8d3bf7e9b3d7de772d6e30ec6891ed88dca77ffb
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-06-30 20:46:34 +0200

    csharp(hyperinflation-hits-hyperia): solution

--- csharp/hyperinflation-hits-hyperia/HyperinflationHitsHyperia.cs
+++ csharp/hyperinflation-hits-hyperia/HyperinflationHitsHyperia.cs
@@ -1,19 +1,32 @@
 using System;
+using System.Globalization;
 
 public static class CentralBank
 {
     public static string DisplayDenomination(long @base, long multiplier)
     {
-        throw new NotImplementedException($"Please implement the (static) CentralBank.DisplayDenomination() method");
+        try
+        {
+            return checked(@base * multiplier).ToString();
+        }
+        catch (OverflowException)
+        {
+            return "*** Too Big ***";
+        }
     }
 
-    public static string DisplayGDP(float @base, float multiplier)
-    {
-        throw new NotImplementedException($"Please implement the (static) CentralBank.DisplayGDP() method");
-    }
+    public static string DisplayGDP(float @base, float multiplier) =>
+        DisplayDenomination((long)@base, (long)multiplier);
 
     public static string DisplayChiefEconomistSalary(decimal salaryBase, decimal multiplier)
     {
-        throw new NotImplementedException($"Please implement the (static) CentralBank.DisplayChiefEconomistSalary() method");
+        try
+        {
+            return (salaryBase * multiplier).ToString(CultureInfo.InvariantCulture);
+        }
+        catch (OverflowException)
+        {
+            return "*** Much Too Big ***";
+        }
     }
 }
