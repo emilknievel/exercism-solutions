e8137834c0f5382dad3dbd8d4de8d1fcbe997066
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-07-28 13:31:33 +0200

    csharp(error-handling): solution

--- csharp/error-handling/ErrorHandling.cs
+++ csharp/error-handling/ErrorHandling.cs
@@ -2,23 +2,14 @@ using System;
 
 public static class ErrorHandling
 {
-    public static void HandleErrorByThrowingException()
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static void HandleErrorByThrowingException() => throw new Exception();
 
-    public static int? HandleErrorByReturningNullableType(string input)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static int? HandleErrorByReturningNullableType(string input) => int.TryParse(input, out var result) ? result : null;
 
-    public static bool HandleErrorWithOutParam(string input, out int result)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static bool HandleErrorWithOutParam(string input, out int result) => int.TryParse(input, out result);
 
     public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        using (disposableObject) throw new Exception();
     }
 }
