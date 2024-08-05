6c105ffa9f809366803cb0197dffbd5965328564
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-08-05 15:41:12 +0200

    csharp(developer-privileges): solution

--- csharp/developer-privileges/DeveloperPrivileges.cs
+++ csharp/developer-privileges/DeveloperPrivileges.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 
 public class Authenticator
 {
-    // TODO: Implement the Authenticator.Admin property
-    public Identity Admin { get; }
+    public Identity Admin => new()
+    {
+        Email = "admin@ex.ism",
+        FacialFeatures = new FacialFeatures { EyeColor = "green", PhiltrumWidth = 0.9M },
+        NameAndAddress = ["Chanakya", "Mumbai", "India"]
+    };
 
-    // TODO: Implement the Authenticator.Developers property
-    public IDictionary<string, Identity> Developers { get; }
+    public IDictionary<string, Identity> Developers => new Dictionary<string, Identity>
+    {
+        ["Bertrand"] = new()
+        {
+            Email = "bert@ex.ism",
+            FacialFeatures = new FacialFeatures { EyeColor = "blue", PhiltrumWidth = 0.8M },
+            NameAndAddress = ["Bertrand", "Paris", "France"]
+        },
+        ["Anders"] = new()
+        {
+            Email = "anders@ex.ism",
+            FacialFeatures = new FacialFeatures { EyeColor = "brown", PhiltrumWidth = 0.85M },
+            NameAndAddress = ["Anders", "Redmond", "USA"]
+        }
+    };
 }
 
 //**** please do not modify the FacialFeatures class ****
