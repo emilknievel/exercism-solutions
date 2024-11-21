312abbf00edeeb19030183b4ac8103e0905eb388
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-11-21 22:53:16 +0100

    csharp(armstrong-numbers): solution by math definition

--- csharp/armstrong-numbers/ArmstrongNumbers.cs
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -4,6 +4,17 @@ public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        const int numberBase = 10;
+        var digitsInNumber = (int)Math.Log10(number) + 1;
+        var armstrong = 0;
+        for (int i = 0; i < digitsInNumber; i++)
+        {
+            armstrong +=
+                (int)Math.Pow(
+                    (number % Math.Pow(numberBase, i + 1) - number % Math.Pow(numberBase, i)) / Math.Pow(numberBase, i),
+                    digitsInNumber);
+        }
+
+        return armstrong == number;
     }
 }
