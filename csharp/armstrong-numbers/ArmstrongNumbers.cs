36aa53345e12c39b4616e0dc3b5859b6aa73afd3
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-11-21 23:02:31 +0100

    csharp(armstrong-numbers): solution, LINQ

--- csharp/armstrong-numbers/ArmstrongNumbers.cs
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -1,20 +1,11 @@
 using System;
+using System.Linq;
 
 public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        const int numberBase = 10;
-        var digitsInNumber = (int)Math.Log10(number) + 1;
-        var armstrong = 0;
-        for (int i = 0; i < digitsInNumber; i++)
-        {
-            armstrong +=
-                (int)Math.Pow(
-                    (number % Math.Pow(numberBase, i + 1) - number % Math.Pow(numberBase, i)) / Math.Pow(numberBase, i),
-                    digitsInNumber);
-        }
-
-        return armstrong == number;
+        var numberOfDigits = number.ToString().Length;
+        return (int)number.ToString().Sum(num => Math.Pow(char.GetNumericValue(num), numberOfDigits)) == number;
     }
 }
