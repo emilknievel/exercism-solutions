a66e4a6cac1881b1375404ad760b59b9d8279973
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-01-02 20:51:02 +0100

    csharp(high-scores): solution

---
+++ csharp/high-scores/HighScores.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class HighScores(List<int> list)
+{
+    public List<int> Scores() => list;
+    public int Latest() => list[^1];
+    public int PersonalBest() => list.Max();
+    public List<int> PersonalTopThree() => list.OrderDescending().Take(3).ToList();
+}
