8d0560631d8cfe28c40a3b6313221c0e71db4021
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-01-03 15:12:17 +0100

    csharp(hamming): solution

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Linq;
 
 public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (firstStrand.Length != secondStrand.Length)
+            throw new ArgumentException("Strings must have the same length.");
+
+        return firstStrand.Zip(secondStrand).Count(tuple => tuple.First != tuple.Second);
     }
 }
