14a24db6c51e1b29dd2c66a80794f1a32a54fe6b
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-01-13 22:14:09 +0100

    csharp(circular-buffer): solution

--- csharp/circular-buffer/CircularBuffer.cs
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -1,29 +1,50 @@
 using System;
 
-public class CircularBuffer<T>
+public class CircularBuffer<T>(int capacity)
 {
-    public CircularBuffer(int capacity)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    private readonly T[] _buffer = new T[capacity];
+    private int _indexFirst;
+    private int _count;
 
     public T Read()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (_count == 0)
+            throw new InvalidOperationException("Buffer is empty.");
+
+        var result = _buffer[_indexFirst];
+        Clear();
+        return result;
     }
 
     public void Write(T value)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (_count == _buffer.Length)
+            throw new InvalidOperationException("Buffer is full.");
+
+        _buffer[(_indexFirst + _count) % _buffer.Length] = value;
+        _count++;
     }
 
     public void Overwrite(T value)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (_count < _buffer.Length)
+        {
+            Write(value);
+            return;
+        }
+
+        _buffer[_indexFirst] = value;
+        _indexFirst = (_indexFirst + 1) % _buffer.Length;
     }
 
     public void Clear()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (_count <= 0)
+        {
+            return;
+        }
+
+        _indexFirst = (_indexFirst + 1) % _buffer.Length;
+        _count--;
     }
 }
