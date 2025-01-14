8c9d4cb2eac7e8c34aa004a47ca25124b580e0ee
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-01-14 22:35:18 +0100

    csharp(clock): solution
    This solution utilizes the immutable characteristics of a C# `record`
    and contains a `TimeOnly` property for storing the time. The built-in
    `AddHours`, `AddMinutes` methods take care of any wrapping.

--- csharp/clock/Clock.cs
+++ csharp/clock/Clock.cs
@@ -1,19 +1,18 @@
 using System;
 
-public class Clock
+public record Clock
 {
+    private TimeOnly _time = new(0, 0);
+
     public Clock(int hours, int minutes)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        _time = _time.AddHours(hours);
+        _time = _time.AddMinutes(minutes);
     }
 
-    public Clock Add(int minutesToAdd)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public Clock Add(int minutesToAdd) => this with { _time = _time.AddMinutes(minutesToAdd) };
 
-    public Clock Subtract(int minutesToSubtract)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public Clock Subtract(int minutesToSubtract) => this with { _time = _time.AddMinutes(-minutesToSubtract) };
+
+    public override string ToString() => $"{_time.Hour:D2}:{_time.Minute:D2}";
 }
