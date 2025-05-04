65dc59a81d1dae673611b34157e35c1c693122af
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-05-04 21:31:52 +0200

    csharp(isogram): solution, idiomatic approach

--- csharp/isogram/Isogram.cs
+++ csharp/isogram/Isogram.cs
@@ -1,18 +1,8 @@
 public static class Isogram
 {
-    private static char[] _exceptions = [' ', '-'];
-
     public static bool IsIsogram(string word)
     {
-        var seen = new HashSet<char>();
-
-        foreach (char letter in word.ToLower())
-        {
-            if (_exceptions.Contains(letter)) continue;
-            if (seen.Contains(letter)) return false;
-            seen.Add(letter);
-        }
-
-        return true;
+        var lowercaseLetters = word.ToLower().Where(l => char.IsLetter(l)).ToList();
+        return lowercaseLetters.Distinct().Count() == lowercaseLetters.Count();
     }
 }
