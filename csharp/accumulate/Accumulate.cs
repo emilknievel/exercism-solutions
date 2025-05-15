5888fbc65ec0feeb569fd3187c864829579ea696
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-05-15 20:48:52 +0200

    csharp(accumulate): solution

--- csharp/accumulate/Accumulate.cs
+++ csharp/accumulate/Accumulate.cs
@@ -2,6 +2,9 @@ public static class AccumulateExtensions
 {
     public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        foreach (T item in collection)
+        {
+            yield return func(item);
+        }
     }
 }
