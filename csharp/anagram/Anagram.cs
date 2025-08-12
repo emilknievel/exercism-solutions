461b61a241e08bfb0d3dbb097912d4578c3ecc1e
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-08-12 22:46:15 +0200

    csharp(anagram): solution 1
    Initial solution that is pretty straightforward. Nothing fancy.

--- csharp/anagram/Anagram.cs
+++ csharp/anagram/Anagram.cs
@@ -1,12 +1,32 @@
 public class Anagram
 {
+    private static string _baseWordLower = "";
+    private static string _baseWordSorted = "";
+
     public Anagram(string baseWord)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (string.IsNullOrEmpty(baseWord)) return;
+
+        _baseWordLower = baseWord.ToLower();
+        _baseWordSorted = String.Concat(_baseWordLower.OrderBy(c => c));
     }
 
     public string[] FindAnagrams(string[] potentialMatches)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        List<string> anagrams = new();
+
+        foreach (string potentialMatch in potentialMatches)
+        {
+            var potentialMatchLower = potentialMatch.ToLower();
+            if (potentialMatchLower.Equals(_baseWordLower)) continue;
+
+            var sorted = String.Concat(potentialMatchLower.OrderBy(c => c));
+            if (sorted.Equals(_baseWordSorted))
+            {
+                anagrams.Add(potentialMatch);
+            }
+        }
+
+        return anagrams.ToArray();
     }
 }
